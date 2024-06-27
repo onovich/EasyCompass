664b08ab0e6fb3e5dbf6c45a5244f5a912e18a77
onovich <hjn1110@163.com>
2024-06-27 19:05:01 +0800

    <refactor> modify: 底层移除 MapUtil

--- Assets/com.mortise.compass.sample/MapUtil.cs
+++ Assets/com.mortise.compass.sample/MapUtil.cs
@@ -18,7 +18,7 @@ namespace MortiseFrame.Compass {
         static int GetGridIndex(int width, int gridX, int gridY) {
             return gridY * width + gridX;
         }
-
+    
         public static int GetMapHeight(bool[] map, int width) {
             if (width == 0) {
                 return 0;
